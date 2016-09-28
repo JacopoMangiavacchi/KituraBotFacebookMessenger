74d0df3d54752183d4b0724c36c6f510cc488acc
Jacopo Mangiavacchi <jacopo.mangiavacchi@uk.ibm.com>
2016-09-28 16:24:57 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,5 +16,5 @@ let package = Package(
         .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/JacopoMangiavacchi/KituraBot.git", majorVersion: 0),
-        .Package(url: "https://github.com/JacopoMangiavacchi/Kitura-Request.git", majorVersion: 0)
+        .Package(url: "https://github.com/JacopoMangiavacchi/jKitura-Request.git", majorVersion: 0)
     ])
