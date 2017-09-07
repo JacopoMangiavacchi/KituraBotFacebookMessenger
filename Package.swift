5a21cf1f56cd118797f4906c8ffc48ff22d6ed84
Jacopo <jmangia@me.com>
2017-09-06 18:42:25 -0700

    no message

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ import PackageDescription
 let package = Package(
     name: "KituraBotFacebookMessenger",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
-        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 7),
+        .Package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", majorVersion: 3),
         //.Package(url: "https://github.com/IBM-Bluemix/cf-deployment-tracker-client-swift.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/JacopoMangiavacchi/KituraBot.git", majorVersion: 0),
+        .Package(url: "https://github.com/JacopoMangiavacchi/KituraBot.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/JacopoMangiavacchi/jKitura-Request.git", majorVersion: 0)
     ])
